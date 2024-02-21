3546e26c94ba61b68eaa786d80d9b4a4c4e7c0ac
Tohan-B <136444891+Tohan-B@users.noreply.github.com>
2024-02-21 09:17:51 -0800

    Update

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var canvas, ctx, w, h;
 var mousePos;
 
-// an empty array!
 var balls = [];
+var initialNumberOfBalls;
+var globalSpeedMutiplier = 1;
+var colorToEat = "red";
+var wrongBallsEaten = (goodBallsEaten = 0);
+var numberOfGoodBalls;
 
 var player = {
   x: 10,
@@ -13,32 +17,61 @@ var player = {
 };
 
 window.onload = function init() {
-  // called AFTER the page has been loaded
   canvas = document.querySelector("#myCanvas");
 
-  // often useful
   w = canvas.width;
   h = canvas.height;
 
-  // important, we will draw with this object
   ctx = canvas.getContext("2d");
 
-  // create 10 balls
-  balls = createBalls(10);
+  startGame(10);
 
-  // add a mousemove event listener to the canvas
   canvas.addEventListener("mousemove", mouseMoved);
 
-  // ready to go !
   mainLoop();
 };
+
+function startGame(nb) {
+  do {
+    balls = createBalls(nb);
+    initialNumberOfBalls = nb;
+    numberOfGoodBalls = countNumberOfGoodBalls(balls, colorToEat);
+  } while (numberOfGoodBalls === 0);
+
+  wrongBallsEaten = goodBallsEaten = 0;
+}
+
+function countNumberOfGoodBalls(balls, colorToEat) {
+  var nb = 0;
+
+  balls.forEach(function (b) {
+    if (b.color === colorToEat) nb++;
+  });
+
+  return nb;
+}
+
+function changeNbBalls(nb) {
+  startGame(nb);
+}
+
+function changeColorToEat(color) {
+  colorToEat = color;
+}
+
+function changePlayerColor(color) {
+  player.color = color;
+}
+
+function changeBallSpeed(coef) {
+  globalSpeedMutiplier = coef;
+}
 
 function mouseMoved(evt) {
   mousePos = getMousePos(canvas, evt);
 }
 
 function getMousePos(canvas, evt) {
-  // necessary work in the canvas coordinate system
   var rect = canvas.getBoundingClientRect();
   return {
     x: evt.clientX - rect.left,
@@ -54,24 +87,19 @@ function movePlayerWithMouse() {
 }
 
 function mainLoop() {
-  // 1 - clear the canvas
   ctx.clearRect(0, 0, w, h);
 
-  // draw the ball and the player
   drawFilledRectangle(player);
   drawAllBalls(balls);
-  drawNumberOfBallsAlive(balls);
+  drawBallNumbers(balls);
 
-  // animate the ball that is bouncing all over the walls
   moveAllBalls(balls);
 
   movePlayerWithMouse();
 
-  // ask for a new animation frame
   requestAnimationFrame(mainLoop);
 }
 
-// Collisions between rectangle and circle
 function circRectsOverlap(x0, y0, w0, h0, cx, cy, r) {
   var testX = cx;
   var testY = cy;
@@ -83,46 +111,49 @@ function circRectsOverlap(x0, y0, w0, h0, cx, cy, r) {
 }
 
 function createBalls(n) {
-  // empty array
   var ballArray = [];
 
-  // create n balls
   for (var i = 0; i < n; i++) {
     var b = {
       x: w / 2,
       y: h / 2,
-      radius: 5 + 30 * Math.random(), // between 5 and 35
-      speedX: -5 + 10 * Math.random(), // between -5 and + 5
-      speedY: -5 + 10 * Math.random(), // between -5 and + 5
+      radius: 5 + 30 * Math.random(),
+      speedX: -5 + 10 * Math.random(),
+      speedY: -5 + 10 * Math.random(),
       color: getARandomColor(),
     };
-    // add ball b to the array
+
     ballArray.push(b);
   }
-  // returns the array full of randomly created balls
+
   return ballArray;
 }
 
 function getARandomColor() {
   var colors = ["red", "blue", "cyan", "purple", "pink", "green", "yellow"];
-  // a value between 0 and color.length-1
-  // Math.round = rounded value
-  // Math.random() a value between 0 and 1
+
   var colorIndex = Math.round((colors.length - 1) * Math.random());
   var c = colors[colorIndex];
 
-  // return the random color
   return c;
 }
 
-function drawNumberOfBallsAlive(balls) {
+function drawBallNumbers(balls) {
   ctx.save();
-  ctx.font = "30px Arial";
+  ctx.font = "20px Arial";
 
   if (balls.length === 0) {
-    ctx.fillText("YOU WIN!", 20, 30);
+    ctx.fillText("Game Over!", 20, 30);
+  } else if (goodBallsEaten === numberOfGoodBalls) {
+    ctx.fillText(
+      "You Win! Final score : " + (initialNumberOfBalls - wrongBallsEaten),
+      20,
+      30
+    );
   } else {
-    ctx.fillText(balls.length, 20, 30);
+    ctx.fillText("Balls still alive: " + balls.length, 210, 30);
+    ctx.fillText("Good Balls eaten: " + goodBallsEaten, 210, 50);
+    ctx.fillText("Wrong Balls eaten: " + wrongBallsEaten, 210, 70);
   }
   ctx.restore();
 }
@@ -134,11 +165,9 @@ function drawAllBalls(ballArray) {
 }
 
 function moveAllBalls(ballArray) {
-  // iterate on all balls in array
-  ballArray.forEach(function (b, index) {
-    // b is the current ball in the array
-    b.x += b.speedX;
-    b.y += b.speedY;
+  balls.forEach(function (b, index) {
+    b.x += b.speedX * globalSpeedMutiplier;
+    b.y += b.speedY * globalSpeedMutiplier;
 
     testCollisionBallWithWalls(b);
 
@@ -158,80 +187,60 @@ function testCollisionWithPlayer(b, index) {
       b.radius
     )
   ) {
-    // we remove the element located at index
-    // from the balls array
-    // splice: first parameter = starting index
-    //         second parameter = number of elements to remove
+    if (b.color === colorToEat) {
+      goodBallsEaten += 1;
+    } else {
+      wrongBallsEaten += 1;
+    }
+
     balls.splice(index, 1);
   }
 }
 
 function testCollisionBallWithWalls(b) {
-  // COLLISION WITH VERTICAL WALLS ?
   if (b.x + b.radius > w) {
-    // the ball hit the right wall
-    // change horizontal direction
     b.speedX = -b.speedX;
 
-    // put the ball at the collision point
     b.x = w - b.radius;
   } else if (b.x - b.radius < 0) {
-    // the ball hit the left wall
-    // change horizontal direction
     b.speedX = -b.speedX;
 
-    // put the ball at the collision point
     b.x = b.radius;
   }
 
-  // COLLISIONS WTH HORIZONTAL WALLS ?
-  // Not in the else as the ball can touch both
-  // vertical and horizontal walls in corners
   if (b.y + b.radius > h) {
-    // the ball hit the right wall
-    // change horizontal direction
     b.speedY = -b.speedY;
 
-    // put the ball at the collision point
     b.y = h - b.radius;
   } else if (b.y - b.radius < 0) {
-    // the ball hit the left wall
-    // change horizontal direction
     b.speedY = -b.speedY;
 
-    // put the ball at the collision point
     b.Y = b.radius;
   }
 }
 
 function drawFilledRectangle(r) {
-  // GOOD practice: save the context, use 2D trasnformations
   ctx.save();
 
-  // translate the coordinate system, draw relative to it
   ctx.translate(r.x, r.y);
 
   ctx.fillStyle = r.color;
-  // (0, 0) is the top left corner of the monster.
+
   ctx.fillRect(0, 0, r.width, r.height);
 
-  // GOOD practice: restore the context
   ctx.restore();
 }
 
 function drawFilledCircle(c) {
-  // GOOD practice: save the context, use 2D trasnformations
   ctx.save();
 
-  // translate the coordinate system, draw relative to it
   ctx.translate(c.x, c.y);
 
   ctx.fillStyle = c.color;
-  // (0, 0) is the top left corner of the monster.
+
   ctx.beginPath();
   ctx.arc(0, 0, c.radius, 0, 2 * Math.PI);
   ctx.fill();
 
-  // GOOD practice: restore the context
   ctx.restore();
 }
